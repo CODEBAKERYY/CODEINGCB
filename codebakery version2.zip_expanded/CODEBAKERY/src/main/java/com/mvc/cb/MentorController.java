dd1e03fd8899a07bb1d4ae8028f22f4ed1ad41d1
Geon0 <kky8809@gmail.com>
2020-06-17 16:54:33 +0900

    승연이꺼 추가
    추가

--- codebakery version2.zip_expanded/CODEBAKERY/src/main/java/com/mvc/cb/MentorController.java
+++ codebakery version2.zip_expanded/CODEBAKERY/src/main/java/com/mvc/cb/MentorController.java
@@ -8,7 +8,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.mvc.cb.biz.MentorBiz;
-import com.mvc.cb.model.dto.MentorDto;
+import com.mvc.cb.biz.MentorReviewBiz;
 
 @Controller
 public class MentorController {
@@ -17,12 +17,17 @@ public class MentorController {
 
 	@Autowired
 	private MentorBiz m_biz;
+	
+	@Autowired
+	private MentorReviewBiz mr_biz;
 
 	@RequestMapping(value = "/mentor_detailAll.do")
 
 	public String mentorDetailall(Model model) {
 		logger.info("mentor selectAll");
 		model.addAttribute("mentor", m_biz.selectList());
+		logger.info("mentorReview selectAll");
+		model.addAttribute("mentorreview",mr_biz.selectList());
 		return "mentor_detail";
 	}
 
