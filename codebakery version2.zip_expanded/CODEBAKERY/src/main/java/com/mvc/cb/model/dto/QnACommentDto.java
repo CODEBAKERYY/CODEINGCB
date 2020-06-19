4c64f651f368f1d8323173b36edfb27498bce8fe
ssyeon612 <ssyeon612@gmail.com>
2020-06-19 19:15:33 +0900

    질의응답 수정_0619
    질의응답 수정

--- codebakery version2.zip_expanded/CODEBAKERY/src/main/java/com/mvc/cb/model/dto/QnACommentDto.java
+++ codebakery version2.zip_expanded/CODEBAKERY/src/main/java/com/mvc/cb/model/dto/QnACommentDto.java
@@ -4,13 +4,14 @@ import java.util.Date;
 
 public class QnACommentDto {
 
+
 	private int comment_No;
 	private int question_No;
-	private int answer_No;
+	private int parent_No;
 	private String comment_Content;
 	private Date comment_Date;
-	private int group_no;
-	private String reply_no;
+	private int depth;
+	private String reply_Id;
 	private String user_Id;
 	private String nuser_Id;
 
@@ -18,16 +19,16 @@ public class QnACommentDto {
 		super();
 	}
 
-	public QnACommentDto(int comment_No, int question_No, int answer_No, String comment_Content, Date comment_Date,
-			int group_no, String reply_no, String user_Id, String nuser_Id) {
+	public QnACommentDto(int comment_No, int question_No, int parent_No, String comment_Content, Date comment_Date,
+			int depth, String reply_Id, String user_Id, String nuser_Id) {
 		super();
 		this.comment_No = comment_No;
 		this.question_No = question_No;
-		this.answer_No = answer_No;
+		this.parent_No = parent_No;
 		this.comment_Content = comment_Content;
 		this.comment_Date = comment_Date;
-		this.group_no = group_no;
-		this.reply_no = reply_no;
+		this.depth = depth;
+		this.reply_Id = reply_Id;
 		this.user_Id = user_Id;
 		this.nuser_Id = nuser_Id;
 	}
@@ -48,12 +49,12 @@ public class QnACommentDto {
 		this.question_No = question_No;
 	}
 
-	public int getAnswer_No() {
-		return answer_No;
+	public int getParent_No() {
+		return parent_No;
 	}
 
-	public void setAnswer_No(int answer_No) {
-		this.answer_No = answer_No;
+	public void setParent_No(int parent_No) {
+		this.parent_No = parent_No;
 	}
 
 	public String getComment_Content() {
@@ -72,20 +73,20 @@ public class QnACommentDto {
 		this.comment_Date = comment_Date;
 	}
 
-	public int getGroup_no() {
-		return group_no;
+	public int getDepth() {
+		return depth;
 	}
 
-	public void setGroup_no(int group_no) {
-		this.group_no = group_no;
+	public void setDepth(int depth) {
+		this.depth = depth;
 	}
 
-	public String getReply_no() {
-		return reply_no;
+	public String getReply_Id() {
+		return reply_Id;
 	}
 
-	public void setReply_no(String reply_no) {
-		this.reply_no = reply_no;
+	public void setReply_Id(String reply_Id) {
+		this.reply_Id = reply_Id;
 	}
 
 	public String getUser_Id() {
@@ -106,10 +107,11 @@ public class QnACommentDto {
 
 	@Override
 	public String toString() {
-		return "QnACommentDto [comment_No=" + comment_No + ", question_No=" + question_No + ", answer_No=" + answer_No
-				+ ", comment_Content=" + comment_Content + ", comment_Date=" + comment_Date + ", group_no=" + group_no
-				+ ", reply_no=" + reply_no + ", user_Id=" + user_Id + ", nuser_Id=" + nuser_Id + "]";
+		return "QnACommentDto [comment_No=" + comment_No + ", question_No=" + question_No + ", parent_No=" + parent_No
+				+ ", comment_Content=" + comment_Content + ", comment_Date=" + comment_Date + ", depth=" + depth
+				+ ", reply_Id=" + reply_Id + ", user_Id=" + user_Id + ", nuser_Id=" + nuser_Id + "]";
 	}
+
 	
 	
 
