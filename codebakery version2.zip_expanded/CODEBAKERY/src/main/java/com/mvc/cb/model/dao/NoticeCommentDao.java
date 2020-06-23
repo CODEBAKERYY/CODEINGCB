a5ed7492add3c14c6fe19c5c77b3781938606283
adhy02154 <adhy02154@naver.com>
2020-06-23 17:08:52 +0900

    Revert "TEST"
    This reverts commit f19356f23b6f3f858371c3b632924970f110399e.

--- codebakery version2.zip_expanded/CODEBAKERY/src/main/java/com/mvc/cb/model/dao/NoticeCommentDao.java
+++ codebakery version2.zip_expanded/CODEBAKERY/src/main/java/com/mvc/cb/model/dao/NoticeCommentDao.java
@@ -8,7 +8,7 @@ public interface NoticeCommentDao {
 	
 	String NAMESPACE = "notice_comment.";
 	
-	public List<NoticeCommentDto> selectAll();
+	public List<NoticeCommentDto> selectAll(int notice_No);
 	public NoticeCommentDto selectOne(int comment_No);
 	public int insert(NoticeCommentDto dto);
 	public int update(NoticeCommentDto dto);
