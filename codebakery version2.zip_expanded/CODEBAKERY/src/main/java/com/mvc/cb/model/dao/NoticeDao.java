7281aafd1605f3bfeb97cf0a9d7769fa1a1a1f2a
Geon0 <kky8809@gmail.com>
2020-06-23 18:03:23 +0900

    박주혁 추가
    추가함

--- codebakery version2.zip_expanded/CODEBAKERY/src/main/java/com/mvc/cb/model/dao/NoticeDao.java
+++ codebakery version2.zip_expanded/CODEBAKERY/src/main/java/com/mvc/cb/model/dao/NoticeDao.java
@@ -3,18 +3,18 @@ package com.mvc.cb.model.dao;
 import java.util.List;
 
 import com.mvc.cb.model.dto.NoticeDto;
-import com.mvc.cb.model.dto.Pagination;
+import com.mvc.cb.model.dto.PageNavigator;
 
 public interface NoticeDao {
-	
-	String NAMESPACE = "notice.";
-	
-	public List<NoticeDto> selectAll(Pagination pagination);
-	public NoticeDto selectOne(int notice_No);
-	public int insert(NoticeDto dto);
-	public int update(NoticeDto dto);
-	public int delete(int notice_No);
-	public int viewUpdate(int notice_No);
-	public int pageCnt(Pagination pagination);
-	
+   
+   String NAMESPACE = "notice.";
+   
+   public List<NoticeDto> selectAll(PageNavigator paging);
+   public NoticeDto selectOne(int notice_No);
+   public int insert(NoticeDto dto);
+   public int update(NoticeDto dto);
+   public int delete(int notice_No);
+   public int viewUpdate(int notice_No);
+   public int pageCnt();
+   
 }
