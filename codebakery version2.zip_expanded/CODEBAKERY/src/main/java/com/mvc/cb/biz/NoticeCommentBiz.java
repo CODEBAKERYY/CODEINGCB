a6d3f1cd3ba9f6be3b790ca96ec0e445689a7080
adhy02154 <adhy02154@naver.com>
2020-06-23 16:00:15 +0900

    미완성
    미완성

--- codebakery version2.zip_expanded/CODEBAKERY/src/main/java/com/mvc/cb/biz/NoticeCommentBiz.java
+++ codebakery version2.zip_expanded/CODEBAKERY/src/main/java/com/mvc/cb/biz/NoticeCommentBiz.java
@@ -6,7 +6,7 @@ import com.mvc.cb.model.dto.NoticeCommentDto;
 
 public interface NoticeCommentBiz {
 	
-	public List<NoticeCommentDto> selectAll();
+	public List<NoticeCommentDto> selectAll(int notice_No);
 	public NoticeCommentDto selectOne(int comment_No);
 	public int insert(NoticeCommentDto dto);
 	public int update(NoticeCommentDto dto);
